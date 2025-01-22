a0c8d69f7ba5b8a8934e775937306f479b9380cb
vvay <momoopsoops@gmail.com>
2025-01-22 15:42:25 +0800

    fix: 🐛 edit vite setting: add build outdir

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/buddyhouseV2/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
