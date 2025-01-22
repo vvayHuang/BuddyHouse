407ba1f7596ffc1031e1dbddafbd45ec334805ec
vvay <momoopsoops@gmail.com>
2025-01-22 16:32:32 +0800

    test: 💍 edit router and every pages title

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,27 +6,31 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => import('../views/HomeView.vue'),
     },
     {
       path: '/food',
       name: 'food',
       component: () => import('../views/FoodView.vue'),
+      meta: { title: 'Food' },
     },
     {
       path: '/pizza',
       name: 'pizza',
       component: () => import('../views/PizzaView.vue'),
+      meta: { title: 'Pizza' },
     },
     {
       path: '/drink',
       name: 'drink',
       component: () => import('../views/DrinkView.vue'),
+      meta: { title: 'Drink' },
     },
     {
       path: '/FAQ',
       name: 'FAQ',
       component: () => import('../views/FAQView.vue'),
+      meta: { title: 'FAQ' },
     },
   ],
   //點選連結時，可以保持原本的位置在畫面上的頂部
@@ -38,6 +41,10 @@ const router = createRouter({
       return { top: 0 }
     }
   },
+})
+// 網頁頁籤上面會顯示官方名及當下的頁面名稱
+router.beforeEach((to) => {
+  document.title = to.meta.title !== undefined ? `${to.meta.title} - Buddy House` : `Buddy House`
 })
 
 export default router
