12076271159a53f85ac6fc6dc2741f84cd8a306d
wayHuang <88394654+vvayHuang@users.noreply.github.com>
2025-07-10 00:03:10 +0800

    更新 404.html 和 main.js，改進重定向邏輯，新增 404 頁面提示信息。

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// 在 createApp 之前加入以下代碼
-if (sessionStorage.redirect) {
-  const redirect = sessionStorage.redirect
-  delete sessionStorage.redirect
-  router.push(redirect)
+// 在 createApp 之前還原 redirect 參數
+const params = new URLSearchParams(window.location.search)
+const redirect = params.get('redirect')
+if (redirect) {
+  window.history.replaceState(null, '', redirect)
 }
 
 const app = createApp(App)
