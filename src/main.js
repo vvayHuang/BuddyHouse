6d2d1c6b2728fec788f6d9c3c1644aed4f3e63e2
wayHuang <88394654+vvayHuang@users.noreply.github.com>
2025-10-24 17:29:07 +0800

    在 package.json 和 package-lock.json 中添加 @microsoft/clarity 依賴，並在 main.js 中初始化 Clarity 以增強網站的性能分析功能。

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import VueGtag from 'vue-gtag-next'
+import Clarity from '@microsoft/clarity'
+
+// Make sure to add your actual project id instead of "yourProjectId".
+const projectId = 'tv4bb6mxb8'
+
+Clarity.init(projectId)
 
 // 在 createApp 之前還原 redirect 參數
 const params = new URLSearchParams(window.location.search)
